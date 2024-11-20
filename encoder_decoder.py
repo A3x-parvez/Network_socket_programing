8ba27b1beaa95d7323b84f78656777a1f571071c
Parvez <rijwanoolkarim@gmail.com>
2024-11-20 23:44:41 +0530

    encript_decript file add

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,8 +1,21 @@
-encode = {
-    "D" : "S",
-    "a" : "1",
-    "t" : "@",
-    "a" : "5"
-}
-message = input("Ente the message :")
+def encrypt_message(message):
+     # Define the encryption mapping
+    encryption_map = {
+        "D": "S",
+        "a": "5",
+        "t": "@",
+        "e": "#"
+    }
+    
+    # Encrypt the message
+    encrypted_message = ""
+    for char in message:
+        if encryption_map[char]:
+            encrypted_message += encryption_map[char]
+        else:
+            encrypted_message += char
+        # encrypted_message += encryption_map[char] if char in encryption_map else char
+    return encrypted_message
 
+message = input("Enter the message to encrypt: ")
+print("Encrypted message:", encrypt_message(message))
